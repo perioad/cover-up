89c663891be6009b84810ee36e7df5daca5ddd89
perioad <perioalexander@gmail.com>
2023-11-25 18:28:47 +0200

    added pwa

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+    providers: [provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+        })]
+}).catch((err) => console.error(err));
