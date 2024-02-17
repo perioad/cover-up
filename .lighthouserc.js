02f2cdbb16e2c985820dcee8ee391128dc6ad536
Aleksandr Perio <aleksandr_perio@epam.com>
2024-02-17 16:06:35 +0200

    automatically add lighthouse badges to README

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,9 +2,11 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: "./dist/coverup/browser",
+      numberOfRuns: 1,
     },
     upload: {
       target: "filesystem",
+      outputDir: "./lhci",
     },
   },
 };
